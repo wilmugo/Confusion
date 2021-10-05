a4650c868be2b5f66e20ff0332e31fed3eee7eea
Wilfredo Murillo <wilmugo@gmail.com>
2021-10-05 13:23:14 -0400

    se agregaron font-awesome 4.7 y bootstrap-social, tambien el header y footer component

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { Navbar, NavbarBrand, Jumbotron } from "reactstrap";
+
+class Header extends Component {
+  render() {
+    return (
+      <>
+        <Navbar dark>
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Jumbotron>
+          <div className="container">
+            <div className="row row-header">
+              <div className="col-12 col-sm-6">
+                <h1>Ristorante Con Fusion</h1>
+                <p>We take inspiration from the world's best cuissines, and create unique fusion experience. our lipsmacking creations and more</p>
+              </div>
+            </div>
+          </div>
+        </Jumbotron>
+      </>
+    );
+  }
+}
+
+
+export default Header
