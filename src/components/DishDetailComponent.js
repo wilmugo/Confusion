9f6d29d789eb4495629d86d4b8dc54147cb88f02
Wilfredo Murillo <wilmugo@gmail.com>
2021-10-06 13:00:35 -0400

    spa parte 2: agregando breadcrumb de navegacion y colocando el dishdetail en una vista aparte con parametros en el url

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,5 +1,14 @@
 import React from "react";
-import { Card, CardBody, CardImg, CardTitle, CardText } from "reactstrap";
+import {
+  Card,
+  CardBody,
+  CardImg,
+  CardTitle,
+  CardText,
+  Breadcrumb,
+  BreadcrumbItem,
+} from "reactstrap";
+import { Link } from "react-router-dom";
 
 function RenderDish({ dish }) {
   if (dish) {
@@ -48,8 +57,21 @@ const DishDetail = (props) => {
     return (
       <div className="container">
         <div className="row">
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <Link to="/menu">Menu</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+          </Breadcrumb>
+          <div className="col-12">
+            <h3>{props.dish.name}</h3>
+            <hr />
+          </div>
+        </div>
+
+        <div className="row">
           <RenderDish dish={props.dish} />
-          <RenderComments comments={props.dish.comments} />
+          <RenderComments comments={props.comments} />
         </div>
       </div>
     );
