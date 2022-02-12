163b6b22d25efe6bb28fb25550105144376a0b87
Wilfredo Murillo <wilmugo@gmail.com>
2022-02-11 21:29:57 -0400

    agregando redux actions

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -40,8 +40,13 @@ class CommentForm extends Component {
   }
 
   handleSubmit(values) {
-    console.log("Current State is: " + JSON.stringify(values));
-    alert("Current State is: " + JSON.stringify(values));
+    this.toggleModal();
+    this.props.addComment(
+      this.props.dishId,
+      values.rating,
+      values.author,
+      values.comment
+    );
   }
 
   render() {
@@ -140,7 +145,7 @@ function RenderDish({ dish }) {
   }
 }
 
-function RenderComments({ comments }) {
+function RenderComments({ comments, addComment, dishId }) {
   if (comments) {
     const commentsItems = comments.map((item) => {
       const dt = new Date(item.date);
@@ -157,7 +162,7 @@ function RenderComments({ comments }) {
       <div className="col-12 col-md-5 m-1">
         <h4>Comments</h4>
         <ul className="list-unstyled">{commentsItems}</ul>
-        <CommentForm />
+        <CommentForm dishId={dishId} addComment={addComment} />
       </div>
     );
   } else {
@@ -184,7 +189,11 @@ const DishDetail = (props) => {
 
         <div className="row">
           <RenderDish dish={props.dish} />
-          <RenderComments comments={props.comments} />
+          <RenderComments
+            comments={props.comments}
+            addComment={props.addComment}
+            dishId={props.dish.id}
+          />
         </div>
       </div>
     );
